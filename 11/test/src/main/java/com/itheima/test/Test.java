ca25bf8b3a0efe28829684b98c32ecc5bd9e8c3d
wxy <85319384@qq.com>
2018-09-26 19:23:49 +0800

    1

--- 11/test/src/main/java/com/itheima/test/Test.java
+++ 11/test/src/main/java/com/itheima/test/Test.java
@@ -3,6 +3,5 @@ package com.itheima.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world1");
-        System.out.println("hello world2");
     }
 }
